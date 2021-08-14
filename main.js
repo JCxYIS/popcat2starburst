01af1928869eaee689499b559e3a84be28aba155
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2021-08-14 16:08:36 +0800

    那到底是什麼技能啊

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         popcat2starburst
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  KO NO KIRITO DA!
 // @author       JCxYIS
 // @match        https://popcat.click/
@@ -9,19 +9,56 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
-    GM_addStyle ( `
-        @font-face { 
-            font-family: 'sao'; 
-            src:  
-                url('fonts/sao-latin.otf'); 
+    GM_addStyle(`
+        @font-face {
+            font-family: 'sao';
+            src:
+                url('https://raw.githubusercontent.com/JCxYIS/popcat2starburst/master/fonts/sao-latin.otf');
         }
 
         body {
             font-family: 'sao';
         }
+
+        #title {
+            margin: .48763em 0 0 0;
+            /*text-decoration: underline #fff 4px;*/
+            font-size: 80px;
+            font-weight: 487.63;
+            color: #fff;
+            /*-webkit-text-stroke-color: #000;*/
+            /*-webkit-text-stroke-width: 1.5px;*/
+        }
+
+        .counter {
+            font-size: 48.763px;
+            font-weight: 487.63;
+            /*-webkit-text-stroke-width: 1px;*/
+        }
+
+        .p {
+            background-image: url('https://i.imgur.com/lGwPrYG.jpg')
+        }
+
+        .op {
+            background-image: url('https://i.imgur.com/FNXvcK2.gif')
+        }
+
+        .cat-img {
+            max-width: 48763px;
+            background-size: cover;
+        }
     ` );
+
+    var parent = document.getElementsByClassName("cat-img")[0]; //document.getElementById("app");
+    var newtitle = document.createElement('div');
+    newtitle.innerHTML = "STARBURST STREAM";
+    newtitle.id = "title";
+    //newtitle.className = "title"
+    parent.insertBefore(newtitle, parent.childNodes[0]);
+    document.getElementsByTagName("img")[0].remove();
 })();
---
+++ readme.md
@@ -0,0 +1,6 @@
+# popcat2starburst
+
+![](https://i.imgur.com/JDPbsmL.gif)
+
+## Download / Install
+[Greasy Fork](https://greasyfork.org/zh-TW/scripts/430784-popcat2starburst)
