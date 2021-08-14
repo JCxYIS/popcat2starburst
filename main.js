c72d2ee517f5c03f2e0fc59d81054dcc5e86b043
JCxYIS (DIYPC-2020) <jsn811@gmail.com>
2021-08-14 11:45:52 +0800

    test

---
+++ main.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         popcat2starburst
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  KO NO KIRITO DA!
+// @author       JCxYIS
+// @match        https://popcat.click/
+// @icon         https://www.google.com/s2/favicons?domain=popcat.click
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    GM_addStyle ( `
+        body {
+            font-family:    Helvetica, sans-serif !important;
+        }
+    ` );
+})();
